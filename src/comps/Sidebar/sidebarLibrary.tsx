430bc6d9c13725c7b38a13f65a44d238dea84c2b
Nitai <nitaidaud@gmail.com>
2024-07-18 22:58:05 +0300

    "Fix typo in SidebarLibrary: 'Libray' -> 'Library'"

--- src/comps/Sidebar/sidebarLibrary.tsx
+++ src/comps/Sidebar/sidebarLibrary.tsx
@@ -3,7 +3,7 @@ import SidebarLink from './sidebarLink'
 export default function SidebarLibrary() {
   return (
     <div className='text-start mt-10 w-full hidden lg:block'>
-        <h2 className='text-white font-bold mb-3'>Libray</h2>
+        <h2 className='text-white font-bold mb-3'>Library</h2>
 
         <SidebarLink content='Recently Played'/>
 

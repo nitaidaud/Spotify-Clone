a4b6a76c6f5c3a5f65b10173041809cdbde243b2
Nitai <nitaidaud@gmail.com>
2024-07-19 09:51:46 +0300

    Remove console logs for VITE_CLIENTID and VITE_CLIENTSECRET in MainHome component.

--- src/comps/Homepage/mainHome.tsx
+++ src/comps/Homepage/mainHome.tsx
@@ -2,10 +2,6 @@ import FeaturedPlaylist from "./featuredPlaylists";
 import TopCategories from "./topCategories";
 import TrendingNow from "./trendingNow";
 
-console.log("id", import.meta.env.VITE_CLIENTID);
-console.log("secret", import.meta.env.VITE_CLIENTSECRET);
-
-
 export default function MainHome() {
   return (
     <div className="grid lg:grid-cols-2 lg:grid-rows-1 lg:h-full h-screen">
